2c521010695f86b875b090459486bdf5d505c0ce
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-01-12 02:02:46 +0530

    Initial Commit

--- scripts.js
+++ scripts.js
@@ -1,67 +1,52 @@
-let humanScore = 0, computerScore = 0
-let round = 0
+let humanScore = 0, computerScore = 0;
+let round = 0;
 
 function getComputerChoice(){
-    // generate a random number
-    // based on the number assign rock, paper or scissors as computerChoice
-    // return the computerChoice
-    let rand, computerChoice
-    rand = parseInt(Math.random()*3)
-    computerChoice = (rand == 0)? "rock": ((rand == 1)? "paper" : "scissors")
+    // Math.random() * n will give a random number from 0 to (n - 1)
+    let rand, computerChoice;
+    rand = parseInt(Math.random() * 3);
+    computerChoice = (rand == 0)? "rock": ((rand == 1)? "paper" : "scissors");
+    const choiceText = document.querySelector("div.computerChoice");
+    choiceText.textContent = `Computer Chose: ${computerChoice}`;
     return computerChoice
 }
 
+function playRound(button){
+    let humanChoice, computerChoice;
+    humanChoice = button.value;
+    console.log(humanChoice);
+    // computerChoice = getComputerChoice();
 
-function getHumanChoice(){
-    let menu, humanChoice, choice
-    menu = `Type any of the options:
-Rock
-Paper
-Scissors
-    `
-    humanChoice = prompt(menu).toLowerCase()
-    while (!(humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors")) {
-        console.warn("Please check your spelling!")
-        humanChoice = prompt(menu).toLowerCase()
-    }
-        
-    return humanChoice
- 
-}
-
-
-function playRound(humanChoice, computerChoice){
-    console.log(`Round ${++round}`)
-    if ( 
-        humanChoice == "rock" && computerChoice == "scissors" ||
-        humanChoice == "paper" && computerChoice == "rock" ||
-        humanChoice == "scissors" && computerChoice == "paper"
-    ) {
-        humanScore++
-        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
-        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-        console.log(`${humanChoice} beats ${computerChoice}. You win!`)
-    }
-    else if ( humanChoice == computerChoice ) {
-        console.log("It's A Draw!")
-    }
-    else {
-        computerScore++
-        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
-        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-        console.log(`${computerChoice} beats ${humanChoice}. You lose!`)
-    }
+    // console.log(`Round ${++round}`)
+    // if ( 
+    //     humanChoice == "rock" && computerChoice == "scissors" ||
+    //     humanChoice == "paper" && computerChoice == "rock" ||
+    //     humanChoice == "scissors" && computerChoice == "paper"
+    // ) {
+    //     humanScore++
+    //     humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+    //     computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+    //     console.log(`${humanChoice} beats ${computerChoice}. You win!`)
+    // }
+    // else if ( humanChoice == computerChoice ) {
+    //     console.log("It's A Draw!")
+    // }
+    // else {
+    //     computerScore++
+    //     humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+    //     computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+    //     console.log(`${computerChoice} beats ${humanChoice}. You lose!`)
+    // }
         
 }
-
 
 function playGame(){
     console.time("Game Duration")
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice()
-        const computerChoice = getComputerChoice()
-        playRound(humanChoice, computerChoice)
-    }
+    
+    const humanChoice = getHumanChoice()
+    const computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+
     console.timeEnd("Game Duration")
     scoreboard = `SCOREBOARD
     Computer: ${computerScore}
@@ -76,8 +61,11 @@ function playGame(){
         console.log("It's a Draw!")
 }
 
+const buttons = document.querySelectorAll("button.btn");
+buttons.forEach((button) => {
+    button.addEventListener("click", playRound(button));
+});
 
-playGame()
 
 
 
