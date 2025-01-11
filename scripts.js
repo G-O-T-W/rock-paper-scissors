3583401c5da8b2195decb381e1a8eab4685b105c
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-01-12 02:18:22 +0530

    Call playRound function at event 'click'

--- scripts.js
+++ scripts.js
@@ -5,38 +5,37 @@ function getComputerChoice(){
     // Math.random() * n will give a random number from 0 to (n - 1)
     let rand, computerChoice;
     rand = parseInt(Math.random() * 3);
-    computerChoice = (rand == 0)? "rock": ((rand == 1)? "paper" : "scissors");
+    computerChoice = (rand == 0)? "Rock": ((rand == 1)? "Paper" : "Scissors");
     const choiceText = document.querySelector("div.computerChoice");
     choiceText.textContent = `Computer Chose: ${computerChoice}`;
     return computerChoice
 }
 
-function playRound(button){
-    let humanChoice, computerChoice;
-    humanChoice = button.value;
-    console.log(humanChoice);
-    // computerChoice = getComputerChoice();
+function playRound(humanChoice, computerChoice){
+    console.log(humanChoice + " " + computerChoice);
 
-    // console.log(`Round ${++round}`)
-    // if ( 
-    //     humanChoice == "rock" && computerChoice == "scissors" ||
-    //     humanChoice == "paper" && computerChoice == "rock" ||
-    //     humanChoice == "scissors" && computerChoice == "paper"
-    // ) {
-    //     humanScore++
-    //     humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
-    //     computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-    //     console.log(`${humanChoice} beats ${computerChoice}. You win!`)
-    // }
-    // else if ( humanChoice == computerChoice ) {
-    //     console.log("It's A Draw!")
-    // }
-    // else {
-    //     computerScore++
-    //     humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
-    //     computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-    //     console.log(`${computerChoice} beats ${humanChoice}. You lose!`)
-    // }
+    console.log(`Round ${++round}`)
+    if ( humanChoice == computerChoice ) {
+        console.log("It's A Draw!")
+    } else if ( 
+        humanChoice == "Rock" && computerChoice == "Scissors" ||
+        humanChoice == "Paper" && computerChoice == "Rock" ||
+        humanChoice == "Scissors" && computerChoice == "Paper"
+    ) {
+        humanScore++;
+        // console code
+        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+        console.log(`${humanChoice} beats ${computerChoice}. You win!`);
+    }
+    
+    else {
+        computerScore++;
+        //console code
+        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+        console.log(`${computerChoice} beats ${humanChoice}. You lose!`);
+    }
         
 }
 
@@ -61,9 +60,14 @@ function playGame(){
         console.log("It's a Draw!")
 }
 
+let humanChoice, computerChoice;
 const buttons = document.querySelectorAll("button.btn");
 buttons.forEach((button) => {
-    button.addEventListener("click", playRound(button));
+    button.addEventListener("click", function (e) {
+        humanChoice = e.target.value;
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    });
 });
 
 
